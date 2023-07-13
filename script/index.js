c6091d777bdcd6c4430eed15ef26b4ee6c7006dc
ranggadwii <rdwii7424@gmail.com>
2023-07-13 14:14:58 +0700

    Initial commit

---
+++ script/index.js
@@ -0,0 +1,59 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+var app = new Vue({
+  el: '#form1',
+  data: function () {
+    return {
+    email : "",
+    emailBlured : false,
+    valid : false,
+    submitted : false,
+    password:"",
+    passwordBlured:false
+    }
+  },
+
+  methods:{
+
+    validate : function(){
+this.emailBlured = true;
+this.passwordBlured = true;
+if( this.validEmail(this.email) && this.validPassword(this.password)){
+this.valid = true;
+}
+},
+
+validEmail : function(email) {
+   
+var re = /(.+)@(.+){2,}\.(.+){2,}/;
+if(re.test(email.toLowerCase())){
+  return true;
+}
+
+},
+
+validPassword : function(password) {
+   if (password.length > 7) {
+    return true;
+   }
+},
+
+submit : function(){
+this.validate();
+if(this.valid){
+this.submitted = true;
+}
+}
+  }
+});
